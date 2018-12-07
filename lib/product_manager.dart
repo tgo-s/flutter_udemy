8d601e9d6efcaa13d7ff28b7123bb7fabadc938f
Thiago Simionato dos Santos <thiago.santos@sanepar.com.br>
2018-12-07 15:12:58 -0200

    Added producto control. Finished the section 2. How to communicate through functions. Passing data up and more

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './products.dart';
+import './product_control.dart';
 
 class ProductManager extends StatefulWidget {
   final String startingProduct;
@@ -18,25 +19,28 @@ class _ProductManagerState extends State<ProductManager> {
   List<String> _products = [];
 
   @override
-    void initState() {
-      super.initState();
-      _products.add(widget.startingProduct);
-    }
+  void initState() {
+    super.initState();
+    _products.add(widget.startingProduct);
+  }
+
+  @override
+  void didUpdateWidget(ProductManager oldWidget) {
+    super.didUpdateWidget(oldWidget);
+  }
+
+  void _addProduct(String product) {
+    setState(() {
+      _products.add(product);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         Container(
-          child: RaisedButton(
-            color: Theme.of(context).primaryColor,
-            child: Text("Add Product"),
-            onPressed: () {
-              setState(() {
-                _products.add('Advanced Food Tester');
-              });
-            },
-          ),
+          child: ProductControl(_addProduct),
           margin: EdgeInsets.all(10.0),
         ),
         Products(_products)
